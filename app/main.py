5104c52e0dfa91a9ef3ced096b811d88ce25e1b7
harold <harold.k.basa@gmail.com>
2023-02-18 00:12:16 +0100

    setup migrations system with alembic

--- app/main.py
+++ app/main.py
@@ -1,12 +1,6 @@
 from fastapi import FastAPI
 
-from app.database.session import engine
-from app.resource.routes import router as resource_router
-
-from .resource import models
-
-models.Base.metadata.create_all(bind=engine)
-
+from .resource.routes import router as resource_router
 
 app = FastAPI()
 app.include_router(resource_router)
